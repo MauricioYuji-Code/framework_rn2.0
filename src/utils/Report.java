db71e22d392598e1d510fb74450defbc23b2fc3f
MauricioYuji-Code <mauricio.nakandakari2@gmail.com>
2021-10-12 23:08:27 -0300

    upd

--- src/utils/Report.java
+++ src/utils/Report.java
@@ -23,7 +23,8 @@ public class Report {
         this.reports = reports;
     }
 
-    public static String generateHTML(String report_name, String content) {
+    public static String generateHTML(String report_name, String content, ArrayList<Perceptron> r) {
+        String samples_list = Report.generateSampleList(r);
         String html_head = "<!DOCTYPE html>\n" +
                 "<html lang='pt-br'>\n" +
                 "<head>\n" +
@@ -40,12 +41,12 @@ public class Report {
                 "    </div>\n" +
                 "  </header>\n";
 
+        String report_title = "<h1 class='report-title'>Logs de execução</h1>";
+
         String html_body_openning_tag = "<body>\n";
 
         String html_container_openning_tag = "<div class='container'>\n";
         String html_container_closing_tag = "</div>\n";
-
-
 
         String header_content = "  <div class='container'>\n" +
                 "    <div class='nn-data'>\n" +
@@ -53,14 +54,14 @@ public class Report {
                 "        <strong>Training Data:</strong>\n" +
                 "        <ul>\n" +
                 "          <!-- Criar estrutura diferente para os pesos e as amostras, pois são arrays e a forma e exibir os dados é diferente -->\n" +
-                "          <li><span>Samples: </span></li>\n" +
-                "          <li><span>Weights: </span></li>\n" +
-                "          <li><span>Learning Rate: </span></li>\n" +
-                "          <li><span>Predict: </span></li>\n" +
-                "          <li><span>Bias: </span></li>\n" +
+                "          <li><div><span>Samples:" + samples_list + "</span></div></li>\n" +
+                "          <li><span>Weights:" + r.get(0).getInitWeightsValuesReport().get(0) + "</span></li>\n" +
+                "          <li><span>Learning Rate: " + r.get(0).getLearningRateValue() + "</span></li>\n" +
+                "          <li><span>Predict: " + r.get(0).getPredictValue() + "</span></li>\n" +
+                "          <li><span>Bias: " + r.get(0).getBiasValue() + "</span></li>\n" +
                 "          <li><span>Structure: </span></li>\n" +
-                "          <li><span>Network Type: </span></li>\n" +
-                "          <li><span>Activation Function: </span></li>\n" +
+                "          <li><span>Network Type: " + r.get(0).getTypeName() + "</span></li>\n" +
+                "          <li><span>Activation Function: " + r.get(0).getFunctionActivaion() + "</span></li>\n" +
                 "        </ul>\n" +
                 "      </div>\n" +
                 "\n" +
@@ -130,6 +131,7 @@ public class Report {
                 "\n" +
                 "    .nn-data {\n" +
                 "      display: flex;\n" +
+                "      flex-direction: column;\n" +
                 "      width: 100%;\n" +
                 "    }\n" +
                 "\n" +
@@ -165,8 +167,8 @@ public class Report {
                 "      margin-bottom: 15px;\n" +
                 "    }\n" +
                 "\n" +
-                "    .nn-initial-info ul,\n" +
-                "    .training-execution-data ul {\n" +
+                "    .nn-initial-info > ul,\n" +
+                "    .training-execution-data > ul {\n" +
                 "      display: flex;\n" +
                 "      flex-direction: column;\n" +
                 "      width: 100%;\n" +
@@ -225,7 +227,7 @@ public class Report {
         String html_close_tag = "</html>";
 
 
-        return html_head + html_header + html_body_openning_tag  + html_container_openning_tag + header_content + content + html_container_closing_tag + css + html_body_closing_tag + html_close_tag;
+        return html_head + html_header + html_body_openning_tag + html_container_openning_tag + header_content + content + html_container_closing_tag + css + html_body_closing_tag + html_close_tag;
     }
 
 
@@ -256,76 +258,58 @@ public class Report {
         String nn_content = "";
         for (int i = 0; i < r.size(); i++) {
             nn_content += "<div class='nn-iteration-container'>" +
-                    "<strong>Época " + r.get(i).getEpoch() + "- Amostra " + (r.get(i).getSamplePosition()+1) + "</strong>" +
+                    "<strong>Época " + r.get(i).getEpoch() + "- Amostra " + (r.get(i).getSamplePosition() + 1) + "</strong>" +
                     "<div class='nn-iteration-content-container'>" +
                     "<div class='feedworward-container'>" +
                     "<strong style='margin:15px 0'>Feedforward</strong>" +
                     "<span>Input Layer: valores da camada de entrada aqui </span>" +
                     "<span>Pesos (Input layer): valores dos pesos aqui</span>" +
-                    "<span>Valor da somatória: " +  r.get(i).getSumValue() + "</span>" +
+                    "<span>Valor da somatória: " + r.get(i).getSumValue() + "</span>" +
                     "<span>Resultado da função de ativação: valor da função de ativação vai aqui</span>" +
                     "<span>Output: " + r.get(i).getOutputValue() + "</span>" +
-                    "<span>Predict: " +  r.get(i).getPredictStatus() + "</span>" +
+                    "<span>Predict: " + r.get(i).getPredictStatus() + "</span>" +
                     "</div>" +
                     "<div class='backpropagation-container'>" +
                     "<strong style='margin:15px 0'>Backpropagation</strong>" +
                     //"<span>Número de neuronios no perceptron: " +  (r.get(i).getNumberneuron()+1) + "</span>" +
-                    "<span>Valor do erro: " +  r.get(i).getErrorValue() + "</span>" +
+                    "<span>Valor do erro: " + r.get(i).getErrorValue() + "</span>" +
                     "<span>Delta peso: valor do delta peso aqui </span>" +
                     "<span>Novo peso: valor do novo peso aqui</span>" +
-                    "<span>Delta bias: " +  r.get(i).getDeltaBias() + "</span>" +
-                    "<span>Novo bias: " +  r.get(i).getNewBias() + "</span>" +
+                    "<span>Delta bias: " + r.get(i).getDeltaBias() + "</span>" +
+                    "<span>Novo bias: " + r.get(i).getNewBias() + "</span>" +
                     "</div></div></div>";
-
-//            System.out.println(nn_content);
-
-
-            //Amostra atual
-            //            System.out.println("Amostra " + (r.get(i).getSamplePosition()+1));
-            //            //Epoca atual
-            //            System.out.println("Epoca : " + r.get(i).getEpoch() + "\nIteração: " + r.get(i).getRound());
-            //            //Saída atual
-            //            System.out.println("Output: " + r.get(i).getOutputValue());
-            //            //Numero de neuronios no perceptron
-            //            System.out.println("Numero de Neuronios: " + (r.get(i).getNumberneuron()+1));
-            //            //Status da predição
-            //            System.out.println("Status da predição" + r.get(i).getPredictStatus());
-            //            //Valor dos inputs
-            //            System.out.println("Valores do input: " + r.get(0).getInputsValuesReport().get(0)[0]);
-            //            System.out.println("Valores do input: " + r.get(1).getInputsValuesReport().get(0)[1]);
-            //            System.out.println("Valores do input: " + r.get(0).getInputsValuesReport().get(1)[0]);
-            //            System.out.println("Valores do input: " + r.get(1).getInputsValuesReport().get(1)[1]);
-            //            //Valor dos pesos
-            //            System.out.println("Valores do peso inicial: " + r.get(0).getInitWeightsValuesReport().get(0));
-            //            System.out.println("Valores do peso inicial: " + r.get(0).getInitWeightsValuesReport().get(1));
-            //            //Valor dos delta pesos
-            //            System.out.println("Valores do delta pesos: " + r.get(1).getDeltaWeightsValues().get(0));
-            //            System.out.println("Valores do delta pesos: " + r.get(1).getDeltaWeightsValues().get(1));
-            //            //Valor dos novos pesos
-            //            System.out.println("Valores do novo peso: " + r.get(1).getNewWeightsValues().get(0));
-            //            System.out.println("Valores do novo peso: " + r.get(1).getNewWeightsValues().get(1));
-            //            //Valor da somatória
-            //            System.out.println("Valor da somatória: " + r.get(i).getSumValue());
-            //            //Função de ativação
-            //            System.out.println("Função de ativação: " + r.get(i).getFunctionActivaion().name());
-            //            //Valor do erro
-            //            System.out.println("Valor do erro: " + r.get(i).getErrorValue());
-            //            //Valor do delta bias
-            //            System.out.println("Valor do delta bias: " + r.get(i).getDeltaBias());
-            //            //Valor do novo bias
-            //            System.out.println("Valor do novo bias: " + r.get(i).getNewBias());
-
-
         }
 
         return nn_content;
+    }
+
+    public static String generateSampleList(ArrayList<Perceptron> r) {
+        String list_open_tag = "<ul class='sample-list'>";
+        String list_close_tag = "</ul>";
+        String list = "";
+
+        int aux = 0;
+        while (aux != r.get(0).getInputsValuesReport().size()) {
+            list += "<li>";
+            for (int i = 0; i < r.get(0).getInputsValuesReport().get(0).length; i++) {
+                list += r.get(0).getInputsValuesReport().get(aux)[i] + " ";
+            }
+            if (aux != r.get(0).getInputsValuesReport().get(0).length) {
+                list += "</li>";
+            }
+            aux++;
+        }
+
+        System.out.println(list_open_tag + list + list_close_tag);
+
+        return list_open_tag + list + list_close_tag;
     }
 
 
     //Todo Jean - Desenvoler o front nessa função
     public static void report(ArrayList<Perceptron> r, String report_name) {
         String content = generateReportContentHTMl(r);
-        String report_teste = Report.generateHTML(report_name, content);
+        String report_teste = Report.generateHTML(report_name, content, r);
 
         Report.reportPrint(report_name, report_teste);
     }
